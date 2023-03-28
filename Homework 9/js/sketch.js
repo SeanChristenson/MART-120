860ead3973447568d4b8157b4955a1b62ca3b605
SeanChristenson <123426816+SeanChristenson@users.noreply.github.com>
2023-03-27 22:39:21 -0600

    upload
    er

---
+++ Homework 9/js/sketch.js
@@ -0,0 +1,19 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(120,45,78);
+    fill(255, 205, 0)
+    circle(250,100,175);
+    fill(12, 102, 5)
+    rect(220,185,60,180);
+    fill(12,12,12)
+    triangle(225, 400, 208, 200, 70, 300,150);
+    point(200, 75);
+    point(300, 75);
+    line(200, 130, 300, 95);
+    
+}
